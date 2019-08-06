65642cd12bae39ccd6c1f803c1f32b52b8cd9307
Vinod Damle <vinod.damle@consensys.net>
2019-08-06 14:32:45 -0400

    CashToken->ERC20Mintable
    * remove some more references to `CashToken`
    * Tested that `truffle migrate --restart --network develop` works with these changes

--- packages/protocol/migrations/2_deploy_zsc.js
+++ packages/protocol/migrations/2_deploy_zsc.js
@@ -1,12 +1,12 @@
 var BurnVerifier = artifacts.require("BurnVerifier");
 var ZetherVerifier = artifacts.require("ZetherVerifier");
-var CashToken = artifacts.require("CashToken");
+var ERC20Mintable = artifacts.require("ERC20Mintable");
 var ZSC = artifacts.require("ZSC");
 
 // Using first two addresses of Ganache
 module.exports = async function(deployer) {
-    await deployer.deploy(CashToken);
+    await deployer.deploy(ERC20Mintable);
     await deployer.deploy(ZetherVerifier);
     await deployer.deploy(BurnVerifier);
-    deployer.deploy(ZSC, CashToken.address, ZetherVerifier.address, BurnVerifier.address, 3000);
+    deployer.deploy(ZSC, ERC20Mintable.address, ZetherVerifier.address, BurnVerifier.address, 3000);
 };
