8261fdfa166471b31fdaac506e1d40934f3d755a
Jim Zhang <jim.zhang@consensys.net>
2019-08-06 12:57:55 -0400

    Added await in migration script to ensure ZSC is not deployed until the dependent contracts have been deployed

--- packages/protocol/migrations/2_deploy_zsc.js
+++ packages/protocol/migrations/2_deploy_zsc.js
@@ -4,9 +4,9 @@ var CashToken = artifacts.require("CashToken");
 var ZSC = artifacts.require("ZSC");
 
 // Using first two addresses of Ganache
-module.exports = function(deployer) {
-    deployer.deploy(CashToken);
-    deployer.deploy(ZetherVerifier);
-    deployer.deploy(BurnVerifier);
+module.exports = async function(deployer) {
+    await deployer.deploy(CashToken);
+    await deployer.deploy(ZetherVerifier);
+    await deployer.deploy(BurnVerifier);
     deployer.deploy(ZSC, CashToken.address, ZetherVerifier.address, BurnVerifier.address, 3000);
 };
