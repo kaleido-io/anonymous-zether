660bd7c7b3df0458b5bbf1fdf419367b00b0a117
amit panghal <panghalamit1892@gmail.com>
2019-08-08 14:40:44 -0400

    truffle test works now

--- packages/protocol/test/zsc.js
+++ packages/protocol/test/zsc.js
@@ -4,10 +4,10 @@ const utils = require('../../anonymous.js/src/utils/utils.js');
 
 contract("ZSC", async accounts => {
     it("should allow depositing / funding", async () => {
-        let cash = await ERC20Mintable.deployed();
+        let erc20mintable = await ERC20Mintable.deployed();
         let zsc = await ZSC.deployed();
-        await cash.mint(accounts[0], 10000000);
-        let balance = await cash.balanceOf.call(accounts[0]);
+        await erc20mintable.mint(accounts[0], 10000000);
+        let balance = await erc20mintable.balanceOf.call(accounts[0]);
         assert.equal(
             balance,
             10000000,
